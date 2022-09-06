e003acddb5cbf2e984ab66149b353bdd95af9d17
laisrmacedo <laisrodriguesmacedo@gmail.com>
2022-09-06 21:53:46 +0200

    Resoluçao 1, 2 e 3

--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,1 +1,15 @@
-let nacionalidade //= adicione o prompt aqui
+let nacionalidade = prompt("Qual sua nacionalidade?");
+
+if(nacionalidade === "brasileira"){
+    console.log("Você nasceu no BRASIL")
+} else if (nacionalidade === "argentina"){
+    console.log("Você nasceu na ARGENTINA")
+} else if (nacionalidade === "uruguaia"){
+    console.log("Você nasceu no URUGUAI")
+} else if (nacionalidade === "chilena"){
+    console.log("Você nasceu no CHILE")
+} else if (nacionalidade === "colombiana"){
+    console.log("Você nasceu na COLÔMBIA")
+} else {
+    console.log("Nacionalidade não identificada")
+}
